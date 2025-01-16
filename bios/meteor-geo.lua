33abc05de84c4bf2cd1b2aa0fbba24a9e68ca5d2
Dmitry Pimenov <dnpimenov@gmail.com>
2025-01-16 22:28:50 +0300

    meteor-geo.lua

---
+++ bios/meteor-geo.lua
@@ -0,0 +1,64 @@
+assert(xpcall(function()
+
+  local function getComponent(name)
+		local address = component.list(name)()
+		return address and component.proxy(address)
+	end
+  
+	local geolizer = getComponent('geolizer')
+	local modem = getComponent('modem')
+	local eeprom = getComponent('eeprom')
+
+	local INIT_PORT = 1
+	local com_port = 2
+  local auto_scan = {}
+
+  local function dump(o)
+    if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+        if type(k) ~= 'number' then k = '"'..k..'"' end
+        s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+    else
+      return tostring(o)
+    end
+  end
+	-- modem.setWakeMessage('meteor-geo')
+
+	local ACTIONS = {
+		[INIT_PORT] = {
+			id = function(address, port)
+				modem.send(address, port, "port(" .. com_port .. ")")
+			end,
+		},
+		[com_port] = {
+			scan = function(...)
+        local result = geolizer.scan(...)
+        return dump(result)
+      end,
+		},
+	}
+
+  while true do
+    local event, receiverAddress, senderAddress, port, dist, msg = computer.pullSignal()
+    if event == "modem_message" and type(msg) == "string" then
+      local action = ACTIONS[port]
+      if action then
+        msg = decode(drone_id, drone_key, msg)
+        if msg then
+          local func, err = load(msg, nil, nil, setmetatable({},{__index=action}))
+          if func then
+            func, err = xpcall(func, debug.traceback)
+          end
+          if err ~= nil then
+            err = tostring(err)
+            modem.send(senderAddress, drone_port, encode(drone_id, drone_key, ("status%q"):format(err)))
+          end
+        end
+      end
+    end
+  end
+
+end, debug.traceback))
